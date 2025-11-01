39da10868f8521b2cb8f1ec780d0b46f7ef73da8
PaneerSelvam-Eduspot <paneerselvamv4@gmail.com>
2025-11-01 18:00:00 +0530

    successfully modified the id param

--- routes/aliens.js
+++ routes/aliens.js
@@ -10,16 +10,24 @@ const router = express.Router({ mergeParams: true });
 router.get('/', async ( req, res) => {
     try{
         
-        const seriesParam = req.params.series; 
+       const { series, id } = req.params;
         let seriesName = "";
         
-        
-        if (seriesParam === "classic") seriesName = "Ben 10 Classic";
-        else if (seriesParam === "alien-force") seriesName = "Ben 10 Alien Force";
-        else if (seriesParam === "ultimate-alien") seriesName = "Ben 10 Ultimate Alien";
+        if (series === "classic") seriesName = "Ben 10 Classic";
+        else if (series === "alien-force") seriesName = "Ben 10 Alien Force";
+        else if (series === "ultimate-alien") seriesName = "Ben 10 Ultimate Alien";
         
         if (!seriesName) {
             return res.status(404).json({ message: "Invalid series specified in the URL." });
+        }
+
+         if (id) {
+            // If ID is provided, fetch specific alien
+            const alien = await Alien.findOne({ _id: id, series: seriesName });
+            if (!alien) {
+                return res.status(404).json({ message: "Alien not found" });
+            }
+            return res.status(200).json(alien);
         }
 
         const aliens = await Alien.find({ series: seriesName });
