ce1ae6ecf291f32f7342b140d159e1e53607af26
Your Name <you@example.com>
2026-03-17 18:06:38 +0530

    Updated path

--- index.js
+++ index.js
@@ -3,25 +3,32 @@ import bodyParser from 'body-parser'
 import aliensRouter from './routes/aliens.js';
 import connectDB from './config/db.js';
 import cors from 'cors';
+import path from 'path';
 import 'dotenv/config'; 
 
 
 const app = express();
 const PORT = process.env.PORT || 5000; 
+const __dirname = path.resolve();
 
 
 await connectDB();
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type']
+}));
+
 app.use(bodyParser.json());
+app.use('./public/aliens/image', express.static(path.join(__dirname, 'public/aliens/image')));
+app.use('./public/aliens/transformimg', express.static(path.join(__dirname, 'public/aliens/transformimg')));
 
 app.use('/api/v1/:series', aliensRouter); 
 
 app.get('/', (req,res) =>{ 
   res.send('Welcome to the Ben 10 Aliens API!');
 });
-
-
 
 
 app.listen(PORT,() => console.log(`Server Running on port: http://localhost:${PORT}`));
