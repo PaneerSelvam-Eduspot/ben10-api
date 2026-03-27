6c01f77222b61a436d596e938f564ae4521c501b
Your Name <you@example.com>
2026-03-27 18:20:22 +0530

    alienSchema restructured

--- models/Alien.js
+++ models/Alien.js
@@ -3,7 +3,14 @@ import mongoose from 'mongoose';
 const alienSchema = new mongoose.Schema({
   id: {type: Number, required: true, index: true},
   name: { type: String, required: true, index: true},
+  species: { type: String, required: true, index: true},
+  planet: { type: String, required: true, index: true},
+  abilities: { type: String, required: true, index: true},
+  image: { type: String, required: true, index: true},
+  transform: {type: String, required: true, index: true},
   series: { type: String, required: true, index: true},
+  firstAppearance: { type: String, required: true, index: true},
+  description: {type: String, required: true, index: true}
 }, {
   collection: 'ben10aliens',
 });
