d0f71073e1dc4f0105a18cae40246ae16e4b5c97
Your Name <you@example.com>
2026-03-28 14:14:17 +0530

    Changed the path /public

--- index.js
+++ index.js
@@ -20,7 +20,9 @@ app.use(cors({
   allowedHeaders: ['Content-Type']
 }));
 
-app.use(helmet());
+app.use(helmet({
+  crossOriginResourcePolicy: { policy: "cross-origin" }
+}));
 
 app.use(rateLimit({
   windowMs: 15 * 60 * 1000,
@@ -35,7 +37,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/aliens/image', express.static(path.join(__dirname, 'public/aliens/image')));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => { 
   res.send('Welcome to the Ben 10 Aliens API!');
