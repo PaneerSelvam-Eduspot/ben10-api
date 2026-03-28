111a322ca6561db803ecb8cbe9154ed13c16bc1a
Your Name <you@example.com>
2026-03-28 11:54:47 +0530

    image path /public/aliens/image updated

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use('/aliens/image', express.static(path.join(__dirname, 'public/aliens/image')));
 
 app.get('/', (req, res) => { 
   res.send('Welcome to the Ben 10 Aliens API!');
