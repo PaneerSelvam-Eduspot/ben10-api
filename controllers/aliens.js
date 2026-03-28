a042d2679910f97dc2a343227b4e423eff6df57c
Your Name <you@example.com>
2026-03-28 14:13:44 +0530

    Updated the API_BASE_URL

--- controllers/aliens.js
+++ controllers/aliens.js
@@ -22,11 +22,12 @@ export const getAliens = async(req, res) => {
       if (id) mongoQuery.id = id;
 
       const aliens = await Alien.find(mongoQuery);
-
+      
+      const baseUrl = process.env.API_BASE_URL;
       const aliensWithUrls = aliens.map(alien => ({
         ...alien._doc,
-        image: `/public/aliens/image/${alien.image}`,
-        transform: `/public/aliens/transformimg/${alien.transform}`
+        image: `${baseUrl}/public/aliens/image/${alien.image}`,
+        transform: `${baseUrl}/public/aliens/transformimg/${alien.transform}`
       }));
 
       res.status(200).json(aliensWithUrls);
@@ -63,11 +64,12 @@ export const getAlien = async (req,res) => {
       });
 
       if (!alien) return res.status(404).json({ message: 'Alien not found'});
-      
+
+      const baseUrl = process.env.API_BASE_URL;
       res.status(200).json({
         ...alien._doc,
-        image: `/public/aliens/image/${alien.image}`,
-        transform: `/public/aliens/transformimg/${alien.transform}`
+        image: `${baseUrl}/public/aliens/image/${alien.image}`,
+        transform: `${baseUrl}/public/aliens/transformimg/${alien.transform}`
       });
     } catch (error) {
       res.status(500).json({ message: error.message });
